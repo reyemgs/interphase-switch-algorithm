9d03fca4aa4fa38cd50eb1cde1380d1c52318df9
reyemgs <thetortyan@gmail.com>
2020-05-12 15:58:16 +0300

    func_test.py

--- SWITCH/correction.py
+++ SWITCH/correction.py
@@ -1,12 +1,13 @@
+
+# ! Korrection.m
 def correction(
     inp_vector, 
     pbest_vector
 ):
     n = len(inp_vector)
-    
     for i in range(0, n, 2):
         if inp_vector[i] == 1 and inp_vector[i+1] == 1:
             inp_vector[i] = pbest_vector[i]
             inp_vector[i+1] = pbest_vector[i+1]
-    
-    return inp_vector
+
+    return inp_vector 
--- SWITCH/func_test.py
+++ SWITCH/func_test.py
@@ -1,11 +1,14 @@
 import numpy as np
+from cost import cost
 from calcthd import calcthd
 from correction import correction
-vec1 = np.array([1, 1, 1, 4, 5, 6, 7, 8, 1, 10])
-vec2 = np.array([1, 2, 1, 4, 5, 1, 7, 1, 9, 1])
-# thd_sum = np.zeros(11)
-# a, b, c = calcthd(thd_sum)
-# print(a, b, c)
 
-a = correction(vec1, vec2)
-print(a)
+# ! TESTS
+def test_function():
+    # * correction.py testing
+    vec1 = np.array([1, 2, 1, 1, 3, 4, 5, 1, 1, 10])
+    vec2 = np.array([1, 2, 3, 4, 6, 4, 8, 9, 12, 10])
+    a = correction(vec1, vec2)
+    print(a)
+
+test_function()
