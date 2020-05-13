8ef3b83f18441781a99af6261f282f66e734fe2d
reyemgs <thetortyan@gmail.com>
2020-05-13 20:35:51 +0300

    1st work calcthd

--- SWITCH/calcthd.py
+++ SWITCH/calcthd.py
@@ -4,22 +4,15 @@ import numpy as np
 
 def calcthd(wf_sum): # * OscS  
     n = len(wf_sum)  # * N
-<<<<<<< HEAD
-    #wf = np.reshape(wf_sum[0,:], (n, 1))  # * Osc
-    wf = wf_sum.T
+    wf = np.reshape(wf_sum[0:], (n, 1))  # * Osc
+    #wf = wf_sum.T
     cvof_elec = math.sqrt(sum((wf_sum**2) * 0.02 / (n - 1)) / 0.02) # * TrueRMSI
-=======
-    
-    #wf = np.reshape(wf_sum[0:], (n, 1))  # * Osc
-    wf = wf_sum.T
-    #cvof_elec = math.sqrt(sum((wf_sum**2) * 0.02 / (n - 1)) / 0.02) # * TrueRMSI
->>>>>>> 9c41a9a18beb1a0780490e456437de051a78d76d
     
     s = np.zeros((n, 10))
     c = np.zeros((n, 10))
-    a = np.zeros(11)
-    b = np.zeros(11)
-    am = np.zeros(11)
+    a = np.zeros(10)
+    b = np.zeros(10)
+    am = np.zeros(10)
 
     for i in range(0, n):
         for j in range(0, 10):
@@ -35,11 +28,11 @@ def calcthd(wf_sum): # * OscS
     print('\nS: ', np.size(s)) 
     print('\nC: ', np.size(c)) 
 
-    for k in range(1, 12):
-        a[k] = (2 / n) * sum(wf * s[:,k])
-        b[k] = (2 / n) * sum(wfd * c[:,k])
-        #a[k] = (2 / n) * sum(np.dot(wf, s[k]))
-        #b[k] = (2 / n) * sum(np.dot(wf, c[k]))
+    for k in range(0, 10):
+        a[k] = (2 / n) * sum(wf[:,0] * s[:,k])
+        b[k] = (2 / n) * sum(wf[:,0] * c[:,k])
+        #a[k] = (2 / n) * sum(np.dot(wf[:,0], s[:,k]))
+        #b[k] = (2 / n) * sum(np.dot(wf[:,0], c[:,k]))
         am[k] = math.sqrt(a[k] ** 2 + b[k] ** 2) / math.sqrt(2)
         k = k + 1
 
